5e09c01f189db12736a0c288ef09a11de7eb4f8c
HARSH DODIYA <70851344+happy-harsh@users.noreply.github.com>
2023-12-01 11:28:40 +0530

    Add files via upload

---
+++ L1_Task2/index.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    let temp = document.getElementById("input").value;
+    var from = document.getElementById("from-temp-type").value;
+    var to = document.getElementById("to-temp-type").value;
+    console.log(temp);
+}
+
+
